5ef2328029a1a46d849854fcdc329b781e520f76
Adesh-111 <adeshabinesh25@gmail.com>
2024-08-13 21:44:06 +0530

    Little corrections for deployment

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import "./Sidebar.css";
 import { assets } from "../../assets/assets";
-import { Context } from "../../context/context";
+import { Context } from "../../context/Context";
 
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import ContextProvider from "./context/Context.jsx";
+import ContextProvider from "./context/Context";
 
 createRoot(document.getElementById("root")).render(
   <ContextProvider>
